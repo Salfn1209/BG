ef1507632affc8cae5eb4126317d5b20fc3a61fb
Salfn1209 <salfn1209@gmail.com>
2026-02-25 20:04:35 -0600

    Se agrega el SecurityFilterChain

--- src/main/java/com/ProyectoBG/BG/config/SecurityConfig.java
+++ src/main/java/com/ProyectoBG/BG/config/SecurityConfig.java
@@ -1,5 +1,31 @@
 package com.ProyectoBG.BG.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.Customizer;
+
+
+@EnableWebSecurity
+@Configuration
 public class SecurityConfig {
-    
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(Customizer.withDefaults())
+            .httpBasic(Customizer.withDefaults())
+            .formLogin(Customizer.withDefaults())
+            .authorizeHttpRequests((authorize) -> authorize
+            .requestMatchers(
+                "/"
+            ).permitAll()
+                .anyRequest().authenticated()
+            );
+
+        return http.build();
+    }
+
 }
